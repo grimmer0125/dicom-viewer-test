18c369b012dd132a913cb910b7a8c28b2ccb83f2
Grimmer <grimmer0125@gmail.com>
2025-01-15 23:18:41 +0800

    feat: read local DICOM files to render 3 views

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
+// example0 (1st):fixed demo online wado dicom image https://github.com/cornerstonejs/vite-react-cornerstone3d/blob/main/src/App.tsx
+// example1 (2nd version): read local file
+//  https://www.cornerstonejs.org/live-examples/local.html
+//  https://github.com/cornerstonejs/cornerstone3D/blob/3062aecefb4115e44735550afbf9f33996cf8a02/packages/tools/examples/local/index.ts#L140
+//  setupSingleFileView
+// example2 (3rd version): read files to render 3 views
+//  https://www.cornerstonejs.org/live-examples/segmentationvolume
+//  https://github.com/cornerstonejs/cornerstone3D/blob/a283bbc44d8baddc69cdd6ce56720e9c3241654a/packages/adapters/examples/segmentationVolume
+//  setupMultpleViewports
 import { useEffect, useRef, useState } from 'react';
 // import createImageIdsAndCacheMetaData from "./lib/createImageIdsAndCacheMetaData"
+import * as cornerstone from '@cornerstonejs/core';
 import {
   init as csRenderInit,
   Enums,
   RenderingEngine,
   Types /*, volumeLoader, imageLoader, metaData */,
 } from '@cornerstonejs/core';
+import cornerstoneDICOMImageLoader from '@cornerstonejs/dicom-image-loader';
 import * as cornerstoneTools from '@cornerstonejs/tools';
 import { init as csToolsInit } from '@cornerstonejs/tools';
-
-import cornerstoneDICOMImageLoader from '@cornerstonejs/dicom-image-loader';
-
+// import * as csDicomImageLoader from "@cornerstonejs/dicom-image-loader";
 import {
+  addManipulationBindings,
   convertMultiframeImageIds /* initProviders, initVolumeLoader */,
   prefetchMetadataInformation,
 } from './helper';
+import labelmapTools from './labelmapTools';
 // import uids from './uids';
+import { BrushTool } from '@cornerstonejs/tools';
+const { segmentation: csToolsSegmentation } = cornerstoneTools;
 
 const {
   PanTool,
@@ -41,33 +54,94 @@ function App() {
   console.debug('app rendering');
 
   const elementRef = useRef<HTMLDivElement>(null);
+  const element2Ref = useRef<HTMLDivElement>(null);
+  const element3Ref = useRef<HTMLDivElement>(null);
+
   const running = useRef(false);
   const [isInitialized, setIsInitialized] = useState<boolean>(false);
 
   const viewport = useRef(null);
 
+  // const dicomMap = new Map();
+
+  const state = useRef({
+    renderingEngine: null,
+    renderingEngineId: 'MY_RENDERING_ENGINE_ID',
+    toolGroup: null,
+    toolGroupId: 'MY_TOOL_GROUP_ID',
+    viewportIds: ['CT_AXIAL', 'CT_SAGITTAL', 'CT_CORONAL'],
+    volumeId: '',
+    segmentationId: 'LOAD_SEG_ID:' + cornerstone.utilities.uuidv4(),
+    referenceImageIds: [],
+    skipOverlapping: false,
+    segImageIds: [],
+    devConfig: { ...new Map().values().next().value },
+  });
+
   useEffect(() => {
-    console.debug('Rendering engine initializing');
+    if (!elementRef.current || !element2Ref.current || !element3Ref.current) {
+      console.log('Rendering engine not initializing');
+      return;
+    }
 
-    // 1st version
-    const setup = async () => {
-      if (running.current) {
-        return;
-      }
-      running.current = true;
+    console.log('Rendering engine initializing');
 
-      // 2nd
-      // initMetaDataProviders();
-      cornerstoneDICOMImageLoader.init();
-      // initVolumeLoader(); // 2nd
-      // await csRenderInit({ // 2nd
-      //   peerImport,
-      //   ...(config?.core ? config.core : {}),
-      // });
-      await csRenderInit(); //1st
+    const setupMultpleViewports = async () => {
+      state.current.toolGroup = cornerstoneTools.ToolGroupManager.createToolGroup(
+        state.current.toolGroupId,
+      );
+      addManipulationBindings(state.current.toolGroup, {
+        toolMap: labelmapTools.toolMap,
+      });
 
-      await csToolsInit(); //1st/2nd
+      cornerstoneTools.addTool(BrushTool);
 
+      state.current.toolGroup.addToolInstance('CircularBrush', BrushTool.toolName, {
+        activeStrategy: 'FILL_INSIDE_CIRCLE',
+      });
+
+      state.current.toolGroup.setToolActive('CircularBrush', {
+        bindings: [{ mouseButton: cornerstoneTools.Enums.MouseBindings.Primary }],
+      });
+
+      const renderingEngine = new RenderingEngine(state.current.renderingEngineId);
+
+      const viewportInputArray = [
+        {
+          viewportId: state.current.viewportIds[0],
+          type: cornerstone.Enums.ViewportType.ORTHOGRAPHIC,
+          element: elementRef.current,
+          defaultOptions: {
+            orientation: cornerstone.Enums.OrientationAxis.AXIAL,
+            background: [0.2, 0, 0.2] as Types.RGB,
+          },
+        },
+        {
+          viewportId: state.current.viewportIds[1],
+          type: cornerstone.Enums.ViewportType.ORTHOGRAPHIC,
+          element: element2Ref.current,
+          defaultOptions: {
+            orientation: cornerstone.Enums.OrientationAxis.SAGITTAL,
+            background: [0.2, 0, 0.2] as Types.RGB,
+          },
+        },
+        {
+          viewportId: state.current.viewportIds[2],
+          type: cornerstone.Enums.ViewportType.ORTHOGRAPHIC,
+          element: element3Ref.current,
+          defaultOptions: {
+            orientation: cornerstone.Enums.OrientationAxis.CORONAL,
+            background: [0.2, 0, 0.2] as Types.RGB,
+          },
+        },
+      ];
+      console.log('viewportInputArray', viewportInputArray, state.current);
+
+      renderingEngine.setViewports(viewportInputArray);
+      state.current.renderingEngine = renderingEngine;
+    };
+
+    const setupSingleFileView = async () => {
       cornerstoneTools.addTool(PanTool);
       cornerstoneTools.addTool(WindowLevelTool);
       cornerstoneTools.addTool(StackScrollTool);
@@ -111,13 +185,10 @@ function App() {
       toolGroup.setToolActive(StackScrollTool.toolName, {
         bindings: [{ mouseButton: MouseBindings.Wheel }],
       });
-
       // Get Cornerstone imageIds and fetch metadata into RAM
-
       // Instantiate a rendering engine
       const renderingEngineId = 'myRenderingEngine';
       const renderingEngine = new RenderingEngine(renderingEngineId);
-
       // Create a stack viewport
       const viewportId = 'CT_STACK';
       const viewportInput = {
@@ -128,13 +199,37 @@ function App() {
           background: [0.2, 0, 0.2] as Types.RGB,
         },
       };
-
       renderingEngine.enableElement(viewportInput);
-
       // Get the stack viewport that was created
       viewport.current = renderingEngine.getViewport(viewportId); // as unknown as <Types.IStackViewport>;
-
       toolGroup.addViewport(viewportId, renderingEngineId);
+    };
+
+    // 1st version
+    const setup = async () => {
+      if (running.current) {
+        return;
+      }
+      running.current = true;
+
+      // 2nd
+      // initMetaDataProviders();
+      cornerstoneDICOMImageLoader.init();
+      // 2nd: https://github.com/cornerstonejs/cornerstone3D/blob/3062aecefb4115e44735550afbf9f33996cf8a02/packages/tools/examples/local/index.ts#L140,
+      // but it is not need for single file and 3 views about multiple files. Commenting out does not matter
+      // initVolumeLoader();
+
+      // await csRenderInit({ // 2nd
+      //   peerImport,
+      //   ...(config?.core ? config.core : {}),
+      // });
+      await csRenderInit(); //1st
+      await csToolsInit(); //1st/2nd
+
+      setupMultpleViewports(); // 3rd
+
+      // 2nd
+      // setupSingleFileView();
 
       // 2nd: for testings, you don't need any of these
       // volumeLoader.registerVolumeLoader('fakeVolumeLoader', fakeVolumeLoader);
@@ -185,7 +280,80 @@ function App() {
     setup();
 
     // Create a stack viewport
-  }, [elementRef, running]);
+  }, [elementRef, element2Ref, element3Ref, running]);
+
+  const getSegmentationIds = () => {
+    return csToolsSegmentation.state.getSegmentations().map((x) => x.segmentationId);
+  };
+
+  const restart = () => {
+    console.debug('Restarting some ui state about multiple file upload');
+    const { volumeId } = state.current;
+
+    if (!volumeId) {
+      return;
+    }
+
+    cornerstone.cache.removeVolumeLoadObject(volumeId);
+
+    csToolsSegmentation.removeAllSegmentationRepresentations();
+
+    const segmentationIds = getSegmentationIds();
+    segmentationIds.forEach((segmentationId) => {
+      csToolsSegmentation.state.removeSegmentation(segmentationId);
+      cornerstone.cache.removeVolumeLoadObject(segmentationId);
+    });
+  };
+  const loadDicomFiles = async () => {
+    restart();
+
+    const volumeLoaderScheme = 'cornerstoneStreamingImageVolume';
+
+    const {
+      volumeId,
+      toolGroup,
+      referenceImageIds,
+      viewportIds,
+      renderingEngineId,
+      renderingEngine,
+    } = state.current;
+
+    const { utilities: csUtilities } = cornerstone;
+
+    state.current.volumeId = volumeLoaderScheme + ':' + csUtilities.uuidv4();
+    const volume = await cornerstone.volumeLoader.createAndCacheVolume(state.current.volumeId, {
+      imageIds: referenceImageIds,
+    });
+
+    toolGroup.addViewport(viewportIds[0], renderingEngineId);
+    toolGroup.addViewport(viewportIds[1], renderingEngineId);
+    toolGroup.addViewport(viewportIds[2], renderingEngineId);
+    await volume.load();
+    await cornerstone.setVolumesForViewports(
+      renderingEngine,
+      [{ volumeId: state.current.volumeId }],
+      viewportIds,
+    );
+    renderingEngine.render();
+  };
+  const readDicomFiles = async (files: File[]) => {
+    // read file
+    // https://github.com/cornerstonejs/cornerstone3D/blob/a283bbc44d8baddc69cdd6ce56720e9c3241654a/packages/adapters/examples/segmentationVolume/utils.ts#L13
+    for (const file of files) {
+      const imageId = cornerstoneDICOMImageLoader.wadouri.fileManager.add(file);
+      await cornerstone.imageLoader.loadAndCacheImage(imageId);
+      // state.referenceImageIds.push(imageId);
+      state.current.referenceImageIds.push(imageId);
+    }
+  };
+
+  const handleMultipleFileUpload = async (
+    event: React.ChangeEvent<HTMLInputElement> | Event,
+  ): Promise<void> => {
+    const files = Array.from((event.target as HTMLInputElement).files || []);
+    await readDicomFiles(files);
+    await loadDicomFiles();
+  };
 
   const handleFileUpload = async (
     event: React.ChangeEvent<HTMLInputElement> | Event,
@@ -399,16 +567,50 @@ function App() {
         className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600'
         disabled={!isInitialized}
       >
+        Select single DICOM file
+      </button>
+
+      <button
+        onClick={() => {
+          const input = document.createElement('input');
+          input.type = 'file';
+          input.multiple = true;
+          input.accept = '.dcm';
+          input.addEventListener('change', handleMultipleFileUpload);
+          input.click();
+        }}
+        className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600'
+        disabled={!isInitialized}
+      >
         Select multiple DICOM files
       </button>
-      <div
-        ref={elementRef}
-        style={{
-          width: '512px',
-          height: '512px',
-          backgroundColor: '#000',
-        }}
-      ></div>
+
+      <div className='flex flex-row'>
+        <div
+          ref={elementRef}
+          style={{
+            width: '512px',
+            height: '512px',
+            backgroundColor: '#000',
+          }}
+        ></div>
+        <div
+          ref={element2Ref}
+          style={{
+            width: '512px',
+            height: '512px',
+            backgroundColor: '#123',
+          }}
+        ></div>
+        <div
+          ref={element3Ref}
+          style={{
+            width: '512px',
+            height: '512px',
+            backgroundColor: '#123',
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
--- src/helper.ts
+++ src/helper.ts
@@ -1,16 +1,26 @@
-import { metaData } from '@cornerstonejs/core';
-import cornerstoneDICOMImageLoader from '@cornerstonejs/dicom-image-loader';
-// import { utilities as csUtils } from '@cornerstonejs/core';
 import {
   cornerstoneStreamingDynamicImageVolumeLoader,
   cornerstoneStreamingImageVolumeLoader,
+  metaData,
   volumeLoader,
 } from '@cornerstonejs/core';
-
+import cornerstoneDICOMImageLoader from '@cornerstonejs/dicom-image-loader';
+import type { Types } from '@cornerstonejs/tools';
+import * as cornerstoneTools from '@cornerstonejs/tools';
 // https://github.com/cornerstonejs/cornerstone3D/blob/main/utils/demo/helpers/initProviders.js#L6
 import * as cornerstone from '@cornerstonejs/core';
-
 import ptScalingMetaDataProvider from './ptScalingMetaDataProvider';
+
+const {
+  LengthTool,
+  StackScrollTool,
+  PanTool,
+  ZoomTool,
+  TrackballRotateTool,
+  Enums: csToolsEnums,
+} = cornerstoneTools;
+
+const { MouseBindings, KeyboardBindings } = csToolsEnums;
 
 const { calibratedPixelSpacingMetadataProvider } = cornerstone.utilities;
 
@@ -116,3 +126,152 @@ export { convertMultiframeImageIds, prefetchMetadataInformation };
 //     await import(/* webpackIgnore: true */ path);
 //     return window[globalName];
 // }
+
+export type ToolBinding = {
+  // A base tool to register.  Should only be defined once per tool
+  tool?: any;
+  // The tool name to base this on
+  baseTool?: string;
+  // The configuration to register with
+  configuration?: Record<string, any>;
+  // Sets to passive initially
+  passive?: boolean;
+  // Initial bindings
+  bindings?: Types.IToolBinding[];
+};
+
+let registered = false;
+export function addManipulationBindings(
+  toolGroup,
+  options: {
+    enableShiftClickZoom?: boolean;
+    is3DViewport?: boolean;
+    toolMap?: Map<string, ToolBinding>;
+  } = {},
+) {
+  const zoomBindings: Types.IToolBinding[] = [
+    {
+      mouseButton: MouseBindings.Secondary,
+    },
+  ];
+
+  const { is3DViewport = false, enableShiftClickZoom = false, toolMap = new Map() } = options;
+
+  if (enableShiftClickZoom === true) {
+    zoomBindings.push({
+      mouseButton: MouseBindings.Primary, // Shift Left Click
+      modifierKey: KeyboardBindings.Shift,
+    });
+  }
+
+  if (!registered) {
+    cornerstoneTools.addTool(PanTool);
+    cornerstoneTools.addTool(ZoomTool);
+    cornerstoneTools.addTool(TrackballRotateTool);
+    cornerstoneTools.addTool(LengthTool);
+    cornerstoneTools.addTool(StackScrollTool);
+    for (const [, config] of toolMap) {
+      if (config.tool) {
+        cornerstoneTools.addTool(config.tool);
+      }
+    }
+  }
+
+  registered = true;
+
+  toolGroup.addTool(PanTool.toolName);
+  // Allow significant zooming to occur
+  toolGroup.addTool(ZoomTool.toolName, {
+    minZoomScale: 0.001,
+    maxZoomScale: 4000,
+  });
+  if (is3DViewport) {
+    toolGroup.addTool(TrackballRotateTool.toolName);
+  } else {
+    toolGroup.addTool(StackScrollTool.toolName);
+  }
+  toolGroup.addTool(LengthTool.toolName);
+  toolGroup.addTool(StackScrollTool.toolName);
+
+  toolGroup.setToolActive(PanTool.toolName, {
+    bindings: [
+      {
+        mouseButton: MouseBindings.Auxiliary,
+      },
+      {
+        numTouchPoints: 1,
+        modifierKey: KeyboardBindings.Ctrl,
+      },
+    ],
+  });
+  toolGroup.setToolActive(ZoomTool.toolName, {
+    bindings: zoomBindings,
+  });
+  // Need a binding to navigate without a wheel mouse
+  toolGroup.setToolActive(StackScrollTool.toolName, {
+    bindings: [
+      {
+        mouseButton: MouseBindings.Primary,
+        modifierKey: KeyboardBindings.Alt,
+      },
+      {
+        numTouchPoints: 1,
+        modifierKey: KeyboardBindings.Alt,
+      },
+      {
+        mouseButton: MouseBindings.Wheel,
+      },
+    ],
+  });
+  // Add a length tool binding to allow testing annotations on examples targetting
+  // other use cases.  Use a primary button with shift+ctrl as that is relatively
+  // unlikely to be otherwise used.
+  toolGroup.setToolActive(LengthTool.toolName, {
+    bindings: [
+      {
+        mouseButton: MouseBindings.Primary,
+        modifierKey: KeyboardBindings.ShiftCtrl,
+      },
+      {
+        numTouchPoints: 1,
+        modifierKey: KeyboardBindings.ShiftCtrl,
+      },
+    ],
+  });
+
+  if (is3DViewport) {
+    toolGroup.setToolActive(TrackballRotateTool.toolName, {
+      bindings: [
+        {
+          mouseButton: MouseBindings.Primary,
+        },
+      ],
+    });
+  } else {
+    toolGroup.setToolActive(StackScrollTool.toolName);
+  }
+
+  // Add extra tools from the toolMap
+  for (const [toolName, config] of toolMap) {
+    if (config.baseTool) {
+      if (!toolGroup.hasTool(config.baseTool)) {
+        toolGroup.addTool(config.baseTool, toolMap.get(config.baseTool)?.configuration);
+      }
+      toolGroup.addToolInstance(toolName, config.baseTool, config.configuration);
+    } else if (!toolGroup.hasTool(toolName)) {
+      toolGroup.addTool(toolName, config.configuration);
+    }
+    if (config.passive) {
+      // This can be applied during add/remove contours
+      toolGroup.setToolPassive(toolName);
+    }
+    if (config.bindings || config.selected) {
+      toolGroup.setToolActive(
+        toolName,
+        (config.bindings && config) || {
+          bindings: [{ mouseButton: MouseBindings.Primary }],
+        },
+      );
+    }
+  }
+}
